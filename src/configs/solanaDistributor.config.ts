b20614fefd42da75fc27f35c15547269c872148d
Adaobi Chuks-Aginam <chuksaginamada@yahoo.com>
2024-09-05 10:58:15 +0100

    Update

--- src/configs/solanaDistributor.config.ts
+++ src/configs/solanaDistributor.config.ts
@@ -1,9 +1,9 @@
-import { ICluster } from "@streamflow/common";
-import { SolanaDistributorClient } from "@streamflow/distributor/solana";
+// import { ICluster } from "@streamflow/common";
+// import { SolanaDistributorClient } from "@streamflow/distributor/solana";
 
-const client = new SolanaDistributorClient({
-    clusterUrl: "https://api.mainnet-beta.solana.com",
-    cluster: ICluster.Mainnet
-});
+// const client = new SolanaDistributorClient({
+//     clusterUrl: "https://api.mainnet-beta.solana.com",
+//     cluster: ICluster.Mainnet
+// });
 
-export default client;
+// export default client;

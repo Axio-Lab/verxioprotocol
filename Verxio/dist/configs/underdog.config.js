504c38a7a58bfad4207fa9063e549b1b679a9fc2
Adaobi Chuks-Aginam <chuksaginamada@yahoo.com>
2024-09-16 16:06:02 +0100

    Remove dist folder and add needed packages

--- Verxio/dist/configs/underdog.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// import axios from "axios";
-// const underdog = axios.create({
-//     baseURL: "https://devnet.underdogprotocol.com",
-//     headers: {
-//         Authorization: `Bearer ${process.env.UNDERDOG_API_KEY}`,
-//     },
-// });
-// export default underdog;

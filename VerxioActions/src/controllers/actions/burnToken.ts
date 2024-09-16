7e399850bf392c17adceb3c35f662208a2a99b36
Adaobi Chuks-Aginam <chuksaginamada@yahoo.com>
2024-09-16 10:34:43 +0100

    Divide the repo into actions and subsequent folders

--- VerxioActions/src/controllers/actions/burnToken.ts
+++ VerxioActions/src/controllers/actions/burnToken.ts
@@ -2,12 +2,12 @@ import { Rpc, createRpc } from '@lightprotocol/stateless.js';
 import { PublicKey, Transaction } from '@solana/web3.js';
 import { createBurnInstruction, getAssociatedTokenAddress } from '@solana/spl-token';
 
-export async function prepareBurnTokensTransaction(mint, amount) {
+export async function prepareBurnTokensTransaction(mint: string, amount: number, pubKey: PublicKey) {
 
   const RPC_ENDPOINT = 'https://devnet.helius-rpc.com/?api-key=d7aa98e6-4f1e-420d-be26-231d5a586b93';
   const COMPRESSION_RPC_ENDPOINT = RPC_ENDPOINT;
   const connection = createRpc(RPC_ENDPOINT, COMPRESSION_RPC_ENDPOINT);
-  account = new PublicKey("");
+  const account = new PublicKey(pubKey);
   
   // Get the associated token account for the owner
   const associatedTokenAddress = await getAssociatedTokenAddress(
---
+++ VerxioActions/src/services/campaign.service.ts
@@ -0,0 +1,18 @@
+import ICampaign from "../interfaces/campaign.interface";
+import Campaign from "../models/campaign.model";
+
+
+export default class CampaignService {
+    async create(campaign: Partial<ICampaign>) {
+        return await Campaign.create(campaign);
+    }
+
+    async findOne(params: {}) {
+        return await Campaign.findOne(params);
+    }
+
+    async find(params: {}) {
+        return await Campaign.find(params);
+    }
+
+}

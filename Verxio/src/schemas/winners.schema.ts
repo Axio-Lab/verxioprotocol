d2acefc8a071b14c6b0c3cd9300a76b56d5e4221
Adaobi Chuks-Aginam <chuksaginamada@yahoo.com>
2024-10-01 06:02:54 +0100

    Feat: Update selectWinners schema

--- Verxio/src/schemas/winners.schema.ts
+++ Verxio/src/schemas/winners.schema.ts
@@ -1,10 +1,8 @@
 import Joi from 'joi';
 
-const objectIdRegex = /^[a-fA-F0-9]{24}$/;
-
 const selectWinnersSchema = Joi.object({
     winners: Joi.array().items(
-        Joi.string().pattern(objectIdRegex).message('"{{#value}}" is not a valid ObjectId')
+        Joi.string()
     ).min(1).required().messages({
         'array.base': 'Winners should be an array.',
         'array.min': 'At least one winner must be provided.',

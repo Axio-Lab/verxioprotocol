f5db9d376a3f161386e92e0dbbf07bb181e3a597
amdonatusprince <amdonatusprince@gmail.com>
2025-05-21 01:57:23 +0100

    fixed the undefined RPC endpoint for mainnet

--- client/src/lib/network-context.tsx
+++ client/src/lib/network-context.tsx
@@ -13,7 +13,7 @@ interface NetworkContextType {
 const NetworkContext = createContext<NetworkContextType | undefined>(undefined)
 
 export const RPC_ENDPOINTS = {
-  devnet: `https://api.devnet.solana.com`,
+  devnet: 'https://api.devnet.solana.com',
   'mainnet-beta': `https://mainnet.helius-rpc.com/?api-key=${process.env.NEXT_PUBLIC_HELIUS_API_KEY}`,
 }
 
